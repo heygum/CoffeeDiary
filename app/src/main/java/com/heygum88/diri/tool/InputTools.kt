8ffdee0d673b93da5525a9491c1b0049b4123a80
heygum <heygum97@gmail.com>
2022-05-24 22:38:01 +0800

    [feature] Update Input format as the same as Output Format

--- app/src/main/java/com/heygum88/diri/tool/InputTools.kt
+++ app/src/main/java/com/heygum88/diri/tool/InputTools.kt
@@ -32,19 +32,19 @@ object InputTools {
 
     private fun appendToDatabase(list: List<String>,db:MDb): Boolean{
 
-        val formatter = SimpleDateFormat("yyyy/MM/dd")
+        val formatter = SimpleDateFormat("yyyy/MM/dd/hh/mm")
         var dates: List<String> = emptyList()
         val sb = StringBuilder()
         for(line in list){
             if(line.contains("////")){
                 if(sb.isNotEmpty() && dates.isNotEmpty()) {
-                    val diary = Diary(date = formatter.parse(dates[0] + '/' + dates[1] + '/' + dates[2]).time
+                    val diary = Diary(date = formatter.parse(dates[0] + '/' + dates[1] + '/' + dates[2] + '/' + dates[3] + '/' + dates[4]).time
                     ,body = sb.toString())
                     db.DiaryDao().insertDiary(diary)
                     sb.clear()
                     dates = emptyList()
                 }
-                dates = line.trim().replace("////","").split(".")
+                dates = line.trim().replace("////","").split("/")
                 if (dates.size < 3) {
                     dates = emptyList()
                 }
